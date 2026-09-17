60c80a9ce4e7362d26e6d62fffcebb2b77c84f35
Arthur <arthur.herc.souza@gmail.com>
2026-09-17 16:55:38 -0300

    iniciar javascript

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+console.log("Olá caras bacanas")
+
+const arthur = "Arthur Herculano"
+
+console.log(arthur)
+
+let fundodetela = "preto"
+
+console.log(fundodetela)
+
+fundodetela = "Branco"
+
+console.log(fundodetela)
+
+if(fundodetela == "Branco"){
+document.body.style.backgroundColor = "white"
+} else if(fundodetela == "preto"){
+    document.body.style.backgroundColor = "black"
+}
